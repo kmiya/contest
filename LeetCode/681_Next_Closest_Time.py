4141d9735b5a695abf5f4d788c0f1cb604633ae8
kmiya <kmiya@outlook.com>
2019-03-05 23:29:11 +0900

    add 681-Next_Closest_Time

---
+++ LeetCode/681_Next_Closest_Time.py
@@ -0,0 +1,19 @@
+# Time: O(1) 4^4 constant solution
+# Space: O(1)
+# Runtime: 44 ms, faster than 39.07% of Python3 online submissions for Next Closest Time.
+# Memory Usage: 13.1 MB, less than 5.55% of Python3 online submissions for Next Closest Time.
+class Solution:
+    def nextClosestTime(self, time: str) -> str:
+        allowed = [time[0], time[1], time[3], time[4]]
+        current = int(time[:2]) * 60 + int(time[3:])
+        result = 24 * 60
+        for h1, h2, m1, m2 in itertools.product(allowed, repeat=4):
+            if (int(h1+h2) >= 24): continue
+            if (int(m1+m2) >= 60): continue
+            comp = int(h1 + h2) * 60 + int(m1+m2)
+            if comp == current: continue
+            result = min(result, (comp - current) % (24 * 60))
+            # print(h1+h2+":"+m1+m2, comp, result)
+        out = (current + result) % (24 * 60)
+        # print(current, result, out)
+        return "{:02}:{:02}".format(out // 60, out % 60)
