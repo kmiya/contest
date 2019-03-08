d5a96b2fabbdf0b5beea0dbc4b8319f480f046e1
kmiya <kmiya@outlook.com>
2019-03-08 23:54:25 +0900

    simplify 1003

--- LeetCode/1003-Check_If_Word_Is_Valid_After_Substitutions.py
+++ LeetCode/1003-Check_If_Word_Is_Valid_After_Substitutions.py
@@ -4,10 +4,6 @@ class Solution:
         jsp = S
         while len(jsp) >= 3:
             jsp = "".join(jsp.split(abc))
-            # print(jsp)
-            if jsp != "" and abc not in jsp:
+            if jsp and abc not in jsp:
                 return False
-        if jsp == "":
-            return True
-        else:
-            return False
+        return True if not jsp else False
