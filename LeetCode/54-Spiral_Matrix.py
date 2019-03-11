4eb57cedabb4f8fa302f491e696f47fc8453465f
kmiya <kmiya@outlook.com>
2019-03-11 23:45:26 +0900

    add 54-Spiral_Matrix

---
+++ LeetCode/54-Spiral_Matrix.py
@@ -0,0 +1,30 @@
+# Time: O(N)
+class Solution:
+    def spiralOrder(self, m: List[List[int]]) -> List[int]:
+        if not m: return m
+        xmin, ymin = 0, 0
+        ans = []
+        xlast, ylast = len(m[0]), len(m)
+        i = 0
+        max_size = xlast * ylast
+        while i < max_size:
+            for xi in range(xmin, xlast):
+                ans.append(m[ymin][xi])
+                i+=1
+            if i >= max_size: break
+            ymin += 1
+            for yi in range(ymin, ylast):
+                ans.append(m[yi][xlast-1])
+                i+=1
+            if i >= max_size: break
+            xlast -= 1
+            for xi in range(xlast, xmin, -1):
+                ans.append(m[ylast-1][xi-1])
+                i+=1
+            if i >= max_size: break
+            ylast -= 1
+            for yi in range(ylast, ymin, -1):
+                ans.append(m[yi-1][xmin])
+                i+=1
+            xmin += 1
+        return ans
