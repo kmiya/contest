dbe5d675066e6ce2e5702d40e76b850b324b9e3c
kmiya <kmiya@outlook.com>
2019-03-29 22:47:00 +0900

    add 125_Valid_Palindrome

---
+++ LeetCode/125_Valid_Palindrome.py
@@ -0,0 +1,9 @@
+# time: O(n)
+# space: O(n), O(1) if one uses (double) pointer(s)
+# Runtime: 64 ms, faster than 68.04% of Python3 online submissions for Valid Palindrome.
+# Memory Usage: 14.8 MB, less than 18.67% of Python3 online submissions for Valid Palindrome.
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t = re.findall('[a-z0-9]*', s.lower())
+        joined = "".join(t)
+        return joined == joined[::-1]
