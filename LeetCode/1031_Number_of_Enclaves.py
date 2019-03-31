9c1ad4f2be78464703d2c61ed39893fab0a76896
kmiya <kmiya@outlook.com>
2019-03-31 19:34:15 +0900

    1031_Number_of_Enclaves

---
+++ LeetCode/1031_Number_of_Enclaves.py
@@ -0,0 +1,23 @@
+# DFS solution
+# time: O(mn)
+# space: O(mn)
+# Runtime: 100 ms, faster than 100.00% of Python3 online submissions for Number of Enclaves.
+# Memory Usage: 15 MB, less than 100.00% of Python3 online submissions for Number of Enclaves.
+class Solution:
+    def numEnclaves(self, A: List[List[int]]) -> int:
+        def rec(x, y):
+            if x < 0 or y < 0 or x >= len(A[0]) or y >= len(A):
+                return
+            if A[y][x]:
+                A[y][x] = 0
+                rec(x,y-1)
+                rec(x-1,y)
+                rec(x+1,y)
+                rec(x,y+1)
+        for x in range(len(A[0])):
+            rec(x,0)
+            rec(x,len(A)-1)
+        for y in range(len(A)):
+            rec(0,y)
+            rec(len(A[0])-1,y)
+        return sum(sum(row) for row in A)
