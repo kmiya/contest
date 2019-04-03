ce7064028d87995c02609aa5310abe07e4b4f2a8
kmiya <kmiya@outlook.com>
2019-04-03 23:10:22 +0900

    add 485-Max_Consecutive_Ones

---
+++ LeetCode/485-Max_Consecutive_Ones.py
@@ -0,0 +1,15 @@
+# time: O(n)
+# space: O(1)
+# Runtime: 92 ms, faster than 49.79% of Python3 online submissions for Max Consecutive Ones.
+# Memory Usage: 13.2 MB, less than 6.50% of Python3 online submissions for Max Consecutive Ones.
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        ans = 0
+        cnt = 0
+        for i in nums:
+            if i:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 0
+        return ans
