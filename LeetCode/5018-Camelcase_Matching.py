47a78b2e50f05f708972a333a7958ea7dcc7ff7e
kmiya <kmiya@outlook.com>
2019-04-07 13:30:17 +0900

    add 5018-Camelcase_Matching

---
+++ LeetCode/5018-Camelcase_Matching.py
@@ -0,0 +1,20 @@
+# RE solution
+# time: average O(n) -- deepends on the complexity of re library
+# time: O(len(queries) + len(pattern)) -- deepends on the complexity of re library
+# Runtime: 48 ms, faster than 100.00% of Python3 online submissions for Camelcase Matching.
+# Memory Usage: 13.2 MB, less than 100.00% of Python3 online submissions for Camelcase Matching.
+class Solution:
+    def camelMatch(self, que: List[str], pat: str) -> List[bool]:
+        np = "[a-z]*"
+        for p in pat:
+            np += p
+            np += "[a-z]*"
+        reg = re.compile(np)
+        ans = []
+        for q in que:
+            res = re.fullmatch(reg,q)
+            if not res:
+                ans.append(False)
+            else:
+                ans.append(True)
+        return ans
