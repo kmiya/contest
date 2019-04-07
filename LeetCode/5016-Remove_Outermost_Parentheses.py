994cd8044d18f4509782da35f4d3ed5e7167c36d
kmiya <kmiya@outlook.com>
2019-04-07 13:29:29 +0900

    add 5016-Remove_Outermost_Parentheses

---
+++ LeetCode/5016-Remove_Outermost_Parentheses.py
@@ -0,0 +1,16 @@
+# time: O(n)
+# space: O(n)
+# Runtime: 44 ms, faster than 100.00% of Python3 online submissions for Remove Outermost Parentheses.
+# Memory Usage: 13.3 MB, less than 100.00% of Python3 online submissions for Remove Outermost Parentheses.
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        l = ""
+        ans = ""
+        cnt = 0
+        for s in S:
+            l += s
+            cnt = cnt + 1 if s == '(' else cnt - 1
+            if not cnt:
+                ans += l[1:-1]
+                l = ""
+        return ans
