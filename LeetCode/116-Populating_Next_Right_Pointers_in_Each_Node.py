0d80259e495e0cd55a8f4ffbff19615117dc40b3
kmiya <kmiya@outlook.com>
2020-03-05 23:59:51 +0900

    add 116-Populating_Next_Right_Pointers_in_Each_Node

---
+++ LeetCode/116-Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,19 @@
+# straightforward recursive solution
+# time: O(#nodes)
+# space: O(#nodes)
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        def rec(left, right):
+            if not left:
+                return
+            left.next = right
+            rec(left.left, left.right)
+            rec(left.right, right.left)
+            rec(right.left, right.right)
+            return
+        if not root:
+            return root
+        rec(root.left, root.right)
+        return root
