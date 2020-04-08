7f5a9a2ef958c1a80671889de19d7dc4a1953ad4
kmiya <kmiya@outlook.com>
2020-04-08 13:40:14 +0900

    add 122-Best_Time_to_Buy_and_Sell_Stock_II

---
+++ LeetCode/122-Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,14 @@
+# Approach #3 in Solution page
+# time: O(#prices)
+# space: O(1)
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]
+        return ans
